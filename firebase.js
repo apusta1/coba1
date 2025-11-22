9298099b765fba9e2a12926740cdffb818966bd9
apusta1 <50144521+apusta1@users.noreply.github.com>
2025-11-22 14:54:21 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBsVdt9s96IxkFm6Cnbo_rT3b_LvkhGl3E",
-  authDomain: "coba1-5a82b.firebaseapp.com",
-  projectId: "coba1-5a82b",
-  storageBucket: "coba1-5a82b.firebasestorage.app",
-  messagingSenderId: "928940485006",
-  appId: "1:928940485006:web:ba945ab505a931168d9cdf",
-  measurementId: "G-9X5J196GV2"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBsVdt9s96IxkFm6Cnbo_rT3b_LvkhGl3E",
+    authDomain: "coba1-5a82b.firebaseapp.com",
+    projectId: "coba1-5a82b",
+    storageBucket: "coba1-5a82b.firebasestorage.app",
+    messagingSenderId: "928940485006",
+    appId: "1:928940485006:web:ba945ab505a931168d9cdf",
+    measurementId: "G-9X5J196GV2"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
